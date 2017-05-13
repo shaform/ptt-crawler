5e484e0905c3763aa414da31aa346315a7b3e1fa
Shaform <shaform@gmail.com>
2017-05-13 14:23:43 +0800

    initial commit

---
+++ ptt_beauty/items.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+
+# Define here the models for your scraped items
+#
+# See documentation in:
+# http://doc.scrapy.org/en/latest/topics/items.html
+
+import scrapy
+
+
+class PostItem(scrapy.Item):
+    title = scrapy.Field()
+    author = scrapy.Field()
+    date = scrapy.Field()
+    content = scrapy.Field()
+    comments = scrapy.Field()
+    score = scrapy.Field()
+    url = scrapy.Field()
+    file_urls = scrapy.Field()
+    files = scrapy.Field()
